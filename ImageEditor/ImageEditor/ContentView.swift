46f7af76bd89a254c8fb6ba4763d19be32bec03f
valilinn <valentinalinchuk@gmail.com>
2022-11-02 23:41:38 +0100

    Added first screen
    Access Your Photos and Videos

--- ImageEditor/ImageEditor/ContentView.swift
+++ ImageEditor/ImageEditor/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                Image("duck")
+                    .resizable()
+                    .frame(width: 170, height: 140)
+                
+                Spacer()
+                    .frame(height: 20)
+                
+                Text("Access Your Photos and Videos")
+                    .font(.title2 .bold())
+                    .foregroundColor(.white)
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                Button("Allow Access") {
+                    
+                }
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .foregroundColor(.white)
+                .frame(width: 380, height: 50)
+                .background(Color.blue)
+                .cornerRadius(10)
+            }
         }
-        .padding()
     }
 }
 
