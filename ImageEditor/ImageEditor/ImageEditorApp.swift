33e3004221da53498bf33b73e733f885ed73cc8c
valilinn <valentinalinchuk@gmail.com>
2022-11-02 19:47:00 +0100

    Add new file Hello world

---
+++ ImageEditor/ImageEditor/ImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditorApp.swift
+//  ImageEditor
+//
+//  Created by Валентина Лінчук on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
